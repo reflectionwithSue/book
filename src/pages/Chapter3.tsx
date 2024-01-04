b53fa9b4423ad8d7708afb6ea895ff811c2447b3
Maryna Mazur <marinemazur@gmail.com>
2024-01-04 15:07:51 +0200

    fix other chapters

--- src/pages/Chapter3.tsx
+++ src/pages/Chapter3.tsx
@@ -1,54 +1,151 @@
 import { LayoutTemplate } from "@/components/LayoutTemplate";
-import { useEffect, useState } from "react";
-import { getText } from "@/api/text";
-import ReactQuill from "react-quill";
-import "quill/dist/quill.bubble.css";
-import { Loader } from "@/components/Loader";
 
 export default function Chapter3() {
-  const [content, setContent] = useState<string>("");
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [title, setTitle] = useState<string>("");
-
-  useEffect(() => {
-    const load = async () => {
-      const response = await getText(3);
-      setContent(response.text);
-      setTitle(response.title);
-      setIsLoading(false);
-    };
-
-    load();
-  }, []);
 
   return (
-    <>
-      {isLoading ? (
-        <div className="w-full h-full flex flex-col justify-center items-center">
-          <Loader />
-        </div>
-      ) : (
-        <LayoutTemplate title={title}>
-          <div className="flex justify-center h-full">
-            <ReactQuill
-              className="quill-custom"
-              theme="bubble"
-              value={content}
-              readOnly={true}
-              style={{
-                width: "100%",
-                height: "100%",
-              }}
-              modules={{
-                clipboard: {
-                  matchVisual: false,
-                },
-              }}
-              
-            />
-          </div>
-        </LayoutTemplate>
-      )}
-    </>
+    <LayoutTemplate title="Режим «Коханка»">
+      <div className="flex justify-center h-full">
+        <article className="w-full overflow-y-auto px-4 flex flex-col content-around">
+          <p className="ch-text mb-2 text-justify hyphens-auto">
+            <span>
+              Сью у жовтому халаті з кублом на голові заколисувала молодшого
+              сина, поки Міша читав казку старшому. Через півгодини малеча
+              заснула.
+            </span>
+          </p>
+          <p className="ch-text mb-2 text-justify hyphens-auto">
+            <span>- Ти як? - поцікавився Міша у дружини.</span>
+          </p>
+          <p className="ch-text mb-2 text-justify hyphens-auto">
+            <span>
+              - Ледь тримаюся на ногах, мрію про ліжечко і солодкий сон. А ти?
+            </span>
+          </p>
+          <p className="ch-text mb-2 text-justify hyphens-auto">
+            <span>
+              - Я теж знесилений. Ого, вже одинадцята година. Час лягати, завтра
+              ж прокидаємося о шостій?
+            </span>
+          </p>
+          <p className="ch-text mb-2 text-justify hyphens-auto">
+            <span>Сью кивнула.</span>
+          </p>
+          <p className="ch-text mb-2 text-justify hyphens-auto">
+            <span>
+              Чоловік пішов у душ, вона прибрала іграшки і помила посуд.
+            </span>
+          </p>
+          <p className="ch-text mb-2 text-justify hyphens-auto">
+            <span>23:30. І куди біжить цей час?</span>
+          </p>
+          <p className="ch-text mb-2 text-justify hyphens-auto">
+            <span>
+              - Люба, ванна вільна, купайся, тільки давай не довго. Пізно вже. Я
+              спати, солоденьких, - чоловік ніжно чмокнув дружину в губи і
+              пірнув у темряву спальні.
+            </span>
+          </p>
+          <p className="ch-text mb-2 text-justify hyphens-auto">
+            <span>
+              Сью відкрила воду. Потім знесилено на автоматі намилила мочалку і
+              покрила піною тіло. Далі витерлась рушником і одягла рожеву
+              піжаму.
+            </span>
+          </p>
+          <p className="ch-text mb-2 text-justify hyphens-auto">
+            <span>
+              - Ти знала, що тобі не пасує рожевий? - раптом почувся голос.
+            </span>
+          </p>
+          <p className="ch-text mb-2 text-justify hyphens-auto">
+            <span>
+              - Що? - здивувалась Сью. - Боже, я так запрацювалася, що в мене
+              вже глюки!
+            </span>
+          </p>
+          <p className="ch-text mb-2 text-justify hyphens-auto">
+            <span>- Я не глюки, я реальна! - обурився голос.</span>
+          </p>
+          <p className="ch-text mb-2 text-justify hyphens-auto">
+            <span>
+              Сью повернулася до дзеркала і побачила там себе. Але не у рожевій
+              піжамі з кублом на голові, а у витонченій спокусливій білизні,
+              покритій мереживом.
+            </span>
+          </p>
+          <p className="ch-text mb-2 text-justify hyphens-auto">
+            <span>
+              - Ой, люба, давай зараз без запитань! Просто мовчи і слухай, -
+              заговорила леді в дзеркалі. - Короче, як би тобі пояснити… У
+              кожної жінки має бути чотири постійні режими, які вона повинна
+              перемикати залежно від ситуації. Це «дівчинка» - ти з цим чудово
+              справляєшся, весело бавлячись у дитячі ігри з синами. Потім
+              «господиня» - тут у тебе теж задовільно, хоча часом ти можеш
+              пересолити деякі страви, але загалом до цього режиму у мене
+              претензій немає. Далі «духовна наставниця» - теж питань немає, ти
+              й справді дуже мудра леді. Ну і четвертий режим - це «коханка».
+              Ось до цієї твоєї ролі у мене купа претензій.
+            </span>
+          </p>
+          <p className="ch-text mb-2 text-justify hyphens-auto">
+            <span>- Я лише... - Сью винувато хотіла виправдатися.</span>
+          </p>
+          <p className="ch-text mb-2 text-justify hyphens-auto">
+            <span>
+              - Стоп, не треба! Я знаю, чому так. Ви вдвох з чоловіком
+              заморочені буденними клопотами, вихованням малюків. От би
+              додаткову годинку в добі, правда?
+            </span>
+          </p>
+          <p className="ch-text mb-2 text-justify hyphens-auto">
+            <span>- Так, було б непогано, - погодилась Сью.</span>
+          </p>
+          <p className="ch-text mb-2 text-justify hyphens-auto">
+            <span>- Дарую! - усміхнулася леді в дзеркалі.</span>
+          </p>
+          <p className="ch-text mb-2 text-justify hyphens-auto">
+            <span>- Не зрозуміла?</span>
+          </p>
+          <p className="ch-text mb-2 text-justify hyphens-auto">
+            <span>
+              - Виходячи з ванної кімнати, просто глянь на годинник і, будь
+              ласка, використай цей час як слід! - підморгнула леді і зникла.
+            </span>
+          </p>
+          <p className="ch-text mb-2 text-justify hyphens-auto">
+            <span>
+              Ну як - зникла. У дзеркалі було відображення Сью в розкішному
+              чорному комплекті білизни, із ідеальним довгим рудим волоссям. Сью
+              перевела погляд з дзеркала на себе.
+            </span>
+          </p>
+          <p className="ch-text mb-2 text-justify hyphens-auto">
+            <span>«Це я? Як… яке в мене розкішне волосся…».</span>
+          </p>
+          <p className="ch-text mb-2 text-justify hyphens-auto">
+            <span>Вона вийшла з ванної і глянула на годинник.</span>
+          </p>
+          <p className="ch-text mb-2 text-justify hyphens-auto">
+            <span>22:00</span>
+          </p>
+          <p className="ch-text mb-2 text-justify hyphens-auto">
+            <span>
+              «Лише десята!» - констатувала дівчина і радісно пішла у спальню.
+              Залізла під ковдру, тихенько обійняла чоловіка.
+            </span>
+          </p>
+          <p className="ch-text mb-2 text-justify hyphens-auto">
+            <span>- Я не сплю, - пошепки відповів він, - чекаю на тебе.</span>
+          </p>
+          <p className="ch-text mb-2 text-justify hyphens-auto">
+            <span>
+              Міша притулився до дружини, відчув божественний аромат її тіла,
+              долоню занурив у волосся і почав вкривати ніжними поцілунками шию,
+              вушко, губи…
+            </span>
+          </p>
+        </article>
+      </div>
+    </LayoutTemplate>
   );
 }
--- src/pages/Chapter4.tsx
+++ src/pages/Chapter4.tsx
@@ -1,55 +1,93 @@
 import { LayoutTemplate } from "@/components/LayoutTemplate";
-import { FC, useEffect, useState } from "react";
-import { getText } from "@/api/text";
-import ReactQuill from "react-quill";
-import "quill/dist/quill.bubble.css";
-import { Loader } from "@/components/Loader";
+import { FC } from "react";
 
 const Chapter4: FC = () => {
-  const [content, setContent] = useState<string>("");
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [title, setTitle] = useState<string>("");
-
-  useEffect(() => {
-    const load = async () => {
-      const response = await getText(4);
-      setContent(response.text);
-      setTitle(response.title);
-      setIsLoading(false);
-    };
-
-    load();
-  }, []);
-
   return (
-    <>
-      {isLoading ? (
-        <div className="w-full h-full flex flex-col justify-center items-center">
-          <Loader />
-        </div>
-      ) : (
-        <LayoutTemplate title={title}>
-          <div className="flex justify-center h-full">
-            <ReactQuill
-              className="quill-custom"
-              theme="bubble"
-              value={content}
-              readOnly={true}
-              style={{
-                width: "100%",
-                height: "100%",
-              }}
-              modules={{
-                clipboard: {
-                  matchVisual: false,
-                },
-              }}
-              
-            />
-          </div>
-        </LayoutTemplate>
-      )}
-    </>
+    <LayoutTemplate title="Вдих-видих. Рівновага">
+      <div className="flex justify-center h-full">
+        <article className="w-full overflow-y-auto px-4 flex flex-col content-around">
+          <p className="ch-text mb-2 text-justify hyphens-auto">
+            Черговий декретний ранок Сью був зовсім не добрим. Її молодший
+            однорічний син вночі дуже погано спав. Сью не виспалася. Старший
+            (дворічний) розбудив її о сьомій ранку заявивши, що хоче їсти.
+          </p>
+          <p className="ch-text mb-2 text-justify hyphens-auto">
+            У піжамі, ще зовсім сонна Сью пішла на кухню. Їсти просили не тільки
+            діти, але й двоє чорних котів. Жінка насипала їм у мисочки сухого
+            корму і почала варити вівсянку. Але замість розпочати трапезу вухаті
+            почали битися. Сью не помітила, як одному з них наступила на хвоста.
+            Кіт заволав, дременув у кімнату. За ним погнався інший. Їхнє шипіння
+            і гарчання розбудили малюка. Хлопчик почав плакати. Сью вимкнула
+            вівсянку і поспішила до сина. Пробігаючи повз залу, наступила на
+            поліцейську машинку. Хрусь!
+          </p>
+          <p className="ch-text mb-2 text-justify hyphens-auto">
+            - От блін, - вилаялася Сью і взяла до рук авто, - по ходу машині
+            гаплик.
+          </p>
+          <p className="ch-text mb-2 text-justify hyphens-auto">
+            - Масині гапік? - повторив за мамою Тимофій і почав плакати.
+          </p>
+          <p className="ch-text mb-2 text-justify hyphens-auto">
+            - Сонечко, - Сью видихала, намагалася бути спокійною і усе правильно
+            пояснити, - мама просила тебе прибирати машинки на місце? От бачиш,
+            що буває, коли авто не на парковці.
+          </p>
+          <p className="ch-text mb-2 text-justify hyphens-auto">
+            Сью обійняла Тимофія, молодший Єгор все ще продовжував кричати.
+            Дівчина дістала малого з ліжечка і почала приколихувати. Потім
+            повернулася на кухню, насипала вівсянки, додала туди сухофруктів і
+            залила молоком, правда, як виявилося, то був кефір. «Тримайся, Сью!
+            Тримайся» - повторювала собі мамочка. Та злість і втома доводили до
+            її до сказу. «Дихай! Вдих. 1. 2. 3. 4. Видих. 1. 2. 3. 4.» Трохи
+            легше. Сью відсунула тарілку і віддала сину свою порцію каші.
+          </p>
+          <p className="ch-text mb-2 text-justify hyphens-auto">
+            Через годину Єгор і коти міцно спали, а Тимофій спокійно грався
+            іграшками. Неочікувано щось загуркотіло на кухні. Сью злякалася, але
+            пішла на звук. О Боже! Все було прибрано, на столі стояли
+            бельгійські вафлі з фруктами, морозивом і карамеллю. Пахло свіжою
+            кавою.
+          </p>
+          <p className="ch-text mb-2 text-justify hyphens-auto">
+            - Сідай. Поснідай нарешті, - почулося позаду. Жінка повернулася і
+            побачила дівчинку. - Без паніки! Ти не спиш. У тебе не глюки. Це я -
+            мала Сью.
+          </p>
+          <p className="ch-text mb-2 text-justify hyphens-auto">
+            - Що ти тут робиш? - витріщила очі доросла Сью. Вона б могла
+            здивуватися такій неочікуваній появі, та за своє життя звикла, що
+            хтось до неї приходить. Вона сама (доросла чи мала), або ж якісь
+            інші істоти.
+          </p>
+          <p className="ch-text mb-2 text-justify hyphens-auto">
+            - У мене не так багато часу, - по-діловому мовила дівчинка, - бо
+            через кілька хвилин повернеться твій чоловік з відрядження, тому
+            слухай. Ти втрачаєш гармонію, рівновагу, а від того й баланс.
+            Занадто віддаєшся материнству. Повністю забула про себе, про свою
+            внутрішню маленьку дівчинку. У тебе має бути відпочинок. Ти ледь
+            стримуєш злість і агресію. Ще кілька таких добрих ранків - і
+            «вдих-видих» тобі не допоможе. Подумай про це.
+          </p>
+          <p className="ch-text mb-2 text-justify hyphens-auto">
+            Сью відкусила шматочок бельгійської вафлі і зробила ковточок кави.
+          </p>
+          <p className="ch-text mb-2 text-justify hyphens-auto">
+            - Ти права, але я не знаю, як я можу собі допомогти, - знизала вона
+            плечима.
+          </p>
+          <p className="ch-text mb-2 text-justify hyphens-auto">
+            - Поговори про це з чоловіком, впевнена, ви щось придумаєте!
+          </p>
+          <p className="ch-text mb-2 text-justify hyphens-auto">
+            Через годину Сью домовилася з Мішею, що раз на два тижні до них
+            будуть приходити з клінінгової фірми. Щоп’ятниці вона йтиме на йогу,
+            а у неділю зустрічатиметься з подругами. А коли відчуватиме, що
+            зовсім безсила - матиме вільний для себе час.
+          </p>
+        </article>
+      </div>
+    </LayoutTemplate>
   );
 };
 
