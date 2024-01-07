a2f7db52187eb504ef27570615ab0e009fe32f97
Maryna Mazur <marinemazur@gmail.com>
2024-01-07 16:17:05 +0200

    change vh on dvh

--- src/components/LayoutTemplate.tsx
+++ src/components/LayoutTemplate.tsx
@@ -20,33 +20,33 @@ export const LayoutTemplate: FC<LayoutsProps> = ({ title, children }) => {
 
   return (
     <div className="layout overflow-y-hidden">
-      <section className="flex flex-col justify-center items-center gap-2 p-3 h-full ">
+      <section className="flex flex-col items-center gap-2 p-3 h-[100dvh] ">
         <HeaderLayoutTemplate />
         {!isLargeDisplay ? (
           <>
-            <main className="h-[65vh] flex flex-col justify-between items-center gap-2 m-0 w-full">
-              <h2 className="text-xl h-[5vh] w-full flex justify-center items-center">
+            <main className="h-[65dvh] flex flex-col justify-between items-center gap-2 m-0 w-full">
+              <h2 className="text-xl h-[5dvh] w-full text-center flex justify-center items-center">
                 {title}
               </h2>
               <div className="flex flex-col w-full">
-                <article className="flex justify-center h-[60vh]">
+                <article className="flex justify-center h-[60dvh]">
                   {children}
                 </article>
               </div>
             </main>
-            <footer className=" sm:h-[8vh]">
+            <footer className=" sm:h-[8dvh]">
               <BgSvg className="absolute -inset-x-5 bottom-0" />
             </footer>
           </>
         ) : (
-          <main className="h-[80vh] flex justify-between items-center gap-10 m-0 w-11/12 p-4">
+          <main className="h-[80dvh] flex justify-between items-center gap-10 m-0 w-11/12 p-4">
             <div className="h-full w-1/2 flex mt-9 justify-center">
               <h2 className="text-[3rem] text-center w-2/3">{title}</h2>
             </div>
-            <article className="flex items-center h-[80vh] w-1/2 mt-8">
+            <article className="flex items-center h-[80dvh] w-1/2 mt-8">
               {children}
             </article>
-            <BgSvg className="h-[38vh] w-[48vw] fixed bottom-0 left-0" />
+            <BgSvg className="h-[38dvh] w-[48vw] fixed bottom-0 left-0" />
           </main>
         )}
       </section>
